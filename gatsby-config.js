f3069cee595f725e575c94f90aedf020943df681
jakecatron <jake.catron@crossroads.net>
2020-02-01 15:01:50 -0500

    add plugins and update meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jake Catron | Front-End Developer`,
+    description: `My slice of the internet.`,
   },
   plugins: [
+    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,22 +16,36 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `JakeCatron`,
+        short_name: `JakeCatron`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#000000`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },  
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+           
+          `montserrat`,
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+          `orbitron`
+        ],
+        display: 'swap'
+      }
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-157140117-1`,
+        head: true
+      },
+    },
   ],
 }
